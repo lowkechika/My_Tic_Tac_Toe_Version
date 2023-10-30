f4444e647b0374572283e0a3d388c92b20c37bf8
John Maroa <jmtagare@gmail.com>
2023-10-30 23:10:20 +0300

    A Second

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+import os
+
 board = {
     1: '1', 2: '2', 3: '3',
     4: '4', 5: '5', 6: '6',
     7: '7', 8: '8', 9: '9',
 }
 
+player_1_moves = 0
+player_2_moves = 0
 player_1_turn = True
 # print(board)
 
@@ -13,15 +17,17 @@ player_1_score = 0
 
 
 def player_1():
-    global player_1_score, player_1_turn
+    global player_1_score, player_1_turn, player_1_moves
     for step in board:
         if step == move:
             if board[step] != 'O' and board[step] != 'X':
                 board[step] = 'X'
-                player_1_score += step
                 player_1_turn = False
+                print('Player 2, Your Turn!')
             else:
                 print('Move already taken')
+                player_1_turn = True
+                print('Player 1, Make a Proper Move!')
 
 
 # player 2
@@ -29,15 +35,18 @@ player_2_score = 0
 
 
 def player_2():
-    global player_2_score, player_1_turn
+
+    global player_2_score, player_1_turn, player_2_moves
     for step in board:
         if step == move:
             if board[step] != 'O' and board[step] != 'X':
                 board[step] = 'O'
-                player_2_score += step
                 player_1_turn = True
+                print('Player 1, Your Turn!')
             else:
                 print('Move already taken')
+                player_1_turn = False
+                print('Player 2, Make a Proper Move!')
 
 
 def live_board():
@@ -56,23 +65,23 @@ print('First time hearing about this Game? Worry Not! Rules here: '
 print("\n")
 print('Welcome to my TicTac version. You should know the rules!')
 print("\n")
+
 if player_1_turn:
     print('Player 1, Your Turn!')
 live_board()
 
-while True:
+active = True
+while active:
     try:
         move = int(input('Enter your move: '))
         if move <= 9:
             if player_1_turn:
-                print('Player 1, Your Turn!')
                 player_1()
-                print('Player 2, Your Turn!')
             else:
-                print('Player 2, Your Turn!')
                 player_2()
 
             live_board()
+
         else:
             print('You shot beyond our limit! Try again with a digit less than 10.')
     except ValueError:
